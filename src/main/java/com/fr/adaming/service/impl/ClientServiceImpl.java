c5ae8f96d3d24099ce8f6ca874dcf214e4bfc8ab
Brias Guillaume <briasguillaume@outlook.fr>
2019-11-14 12:50:26 +0100

    Ajout pakage et interface ClientService et ajout branche gui

---
+++ src/main/java/com/fr/adaming/service/impl/ClientServiceImpl.java
@@ -0,0 +1,40 @@
+package com.fr.adaming.service.impl;
+
+import java.util.List;
+
+import com.fr.adaming.entity.Client;
+import com.fr.adaming.service.ClientService;
+
+public class ClientServiceImpl implements ClientService {
+
+	@Override
+	public Client create(Client client) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client update(Client client) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Client> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
