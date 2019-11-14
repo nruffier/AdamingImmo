506d727d74d935e2d638d9e8a50436021979e614
Brias Guillaume <briasguillaume@outlook.fr>
2019-11-14 14:06:41 +0100

    Ajout de la couche Repository et ajout de la classe ClientServiceImpl

---
+++ src/main/java/com/fr/adaming/repository/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.fr.adaming.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fr.adaming.entity.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+}
--- src/main/java/com/fr/adaming/service/impl/ClientServiceImpl.java
+++ src/main/java/com/fr/adaming/service/impl/ClientServiceImpl.java
@@ -2,39 +2,95 @@ package com.fr.adaming.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+
 import com.fr.adaming.entity.Client;
+import com.fr.adaming.repository.ClientRepository;
 import com.fr.adaming.service.ClientService;
 
+@Service
 public class ClientServiceImpl implements ClientService {
 
+	@Autowired
+	private ClientRepository repo;
+
+	/**
+	 * Save a given Client in the database
+	 * 
+	 * @param Client - the given entity
+	 * @return Client if the given Client doesn't exist in the database - else,
+	 *         return null
+	 */
 	@Override
 	public Client create(Client client) {
-		// TODO Auto-generated method stub
-		return null;
+		Client c = new Client();
+		c.setEmail(client.getEmail());
+		if (repo.exists(Example.of(c))) {
+			return null;
+		} else {
+			return repo.save(client);
+		}
+
 	}
 
+	/**
+	 * Update the values of a given Client
+	 * 
+	 * @param Client - the given entity
+	 * @return Client if the given Client has been modified - else return null
+	 */
 	@Override
 	public Client update(Client client) {
-		// TODO Auto-generated method stub
-		return null;
+		if (repo.existsById(client.getId())) {
+			return repo.save(client);
+		} else {
+			return null;
+		}
 	}
 
+	/**
+	 * Delete the given Client from the database
+	 * 
+	 * @param Integer - the id of the Client you want to delete
+	 * @return true if the Client has been deleted - else return false
+	 */
 	@Override
 	public boolean delete(Integer id) {
-		// TODO Auto-generated method stub
-		return false;
+		if (repo.existsById(id)) {
+			Client client = repo.findById(id).get();
+			repo.delete(client);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
+	/**
+	 * Find all Clients in the database
+	 * 
+	 * @return List of Clients - in case no Client has been inserted, it returns an
+	 *         empty list
+	 */
 	@Override
 	public List<Client> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return repo.findAll();
 	}
 
+	/**
+	 * Find a Client in the database with the given id
+	 * 
+	 * @param id - the given Long id
+	 * @return the Client with the given id if it exists - else return null
+	 */
 	@Override
 	public Client getById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		if (repo.existsById(id)) {
+			return repo.findById(id).get();
+		} else {
+			return null;
+		}
 	}
 
 }
