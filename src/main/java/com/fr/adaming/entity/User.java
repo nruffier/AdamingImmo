5b647a7a7d18d598b96d076aab7969c7d74f7a91
nruffier <n.ruffier74@gmail.com>
2019-11-14 12:02:03 +0100

    Completion de la classe User

--- src/main/java/com/fr/adaming/entity/User.java
+++ src/main/java/com/fr/adaming/entity/User.java
@@ -1,5 +1,40 @@
 package com.fr.adaming.entity;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public abstract class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Email
+	@Column(nullable = false, unique = true)
+	private String email;
+	@Column(nullable = false)
+	private String fullName;
+	@Pattern(regexp = "\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d")
+	private String telephone;
+	
+	public User(@Email String email, String fullName,
+			@Pattern(regexp = "\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d") String telephone) {
+		super();
+		this.email = email;
+		this.fullName = fullName;
+		this.telephone = telephone;
+	}
+	
+	
+	
+	
 }
