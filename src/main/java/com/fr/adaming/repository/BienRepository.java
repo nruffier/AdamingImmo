ec4cde6a40dd0631e3ac5c7a3a59059c7602e31f
bilel <bilel.abbas@gmail.com>
2019-11-14 15:17:46 +0100

    Modification Repository Bien

--- src/main/java/com/fr/adaming/repository/BienRepository.java
+++ src/main/java/com/fr/adaming/repository/BienRepository.java
@@ -1,9 +1,11 @@
 package com.fr.adaming.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fr.adaming.entity.Bien;
 
+@Repository
 public interface BienRepository extends JpaRepository<Bien, Long> {
 
 }
