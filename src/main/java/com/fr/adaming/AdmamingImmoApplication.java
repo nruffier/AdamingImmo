f67b2248530d481c01111f5296ab15bf1f6d44ee
nruffier <n.ruffier74@gmail.com>
2019-11-14 11:36:54 +0100

    Premier push

---
+++ src/main/java/com/fr/adaming/AdmamingImmoApplication.java
@@ -0,0 +1,13 @@
+package com.fr.adaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmamingImmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmamingImmoApplication.class, args);
+	}
+
+}
