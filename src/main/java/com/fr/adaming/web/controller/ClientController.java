3093bb28130d07dad23f182c97a10c0e9c9e44c5
Brias Guillaume <briasguillaume@outlook.fr>
2019-11-20 16:20:00 +0100

    correction de la signature d'une méthode dans ClientController

--- src/main/java/com/fr/adaming/web/controller/ClientController.java
+++ src/main/java/com/fr/adaming/web/controller/ClientController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.fr.adaming.web.dto.ClientDtoUpdate;
  *
  */
 @RequestMapping(path = "api/client")
+@CrossOrigin
 public interface ClientController {
 
 	@PostMapping(path = "/create")
@@ -39,6 +41,6 @@ public interface ClientController {
 	public Client getById(@PathVariable(name = "id") Integer id);
 	
 	@GetMapping(path = "/{idC}/add-agent/{idA}")
-	String addAgent(@PathVariable(name = "idC") Integer idC, @PathVariable(name = "idA") Integer IdA);
+	public String addAgent(@PathVariable(name = "idC") Integer idC, @PathVariable(name = "idA") Integer IdA);
 
 }
